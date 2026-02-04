f60565d63e6d7e525222a4e278f05be91e289ee3
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-02-04 23:23:08 +0530

    add AI function and Package

--- src/Controller/clientController.js
+++ src/Controller/clientController.js
@@ -1,12 +1,30 @@
-
+import { GoogleGenAI } from '@google/genai'
 
 export const AiGetInformation = async(req, res)=>{
+
 
     let {prompt} = req.body;
     console.log(prompt)
     
     try {
-        
+    
+        // AI function import
+        const ai = new GoogleGenAI({
+            apiKey : "AIzaSyD9sdH_1NiT_ImnzfMOT45qiOsrXzRmzEo"
+        })
+        // AI function import
+        const response = await ai.models.generateContent({
+            model: "gemini-3-flash-preview",
+            contents: prompt,
+            config:{
+                systemInstruction : "Please return data in json format"
+            }
+        });
+
+        return res.status(200).json({
+            massage: " Think... AI...",
+            result : response.json()
+        })
         
     } catch (error) {
         
