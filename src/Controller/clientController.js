9577521e82c20658dc55020d452184113b9f2b31
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-02-07 00:52:59 +0530

    use AI model and geting response

--- src/Controller/clientController.js
+++ src/Controller/clientController.js
@@ -1,4 +1,5 @@
 import { GoogleGenAI } from '@google/genai'
+import ClientCaseModel from '../models/ClientCase.js';
 
 export const AiGetInformation = async(req, res)=>{
 
@@ -44,11 +45,69 @@ export const createCaseAIthinking = async(req, res)=>{
     let userId = req.userID;
 
     const {problemStatement, location, caseDate } = req.body;
+    
+    if(!problemStatement || !location || !caseDate){
+        return res.status(404).json({massage: "All Feild Required..."})
+    }
 
     try {
         
-        
+        let clientCase = ClientCaseModel.create({
+            userId,
+            problemStatement,
+            location,
+            caseDate
+        })
 
+        let prompt = `
+
+                You are a legal analysis engine.
+
+                TASK:
+                Analyze the legal case described below and return STRICT JSON ONLY.
+
+                CASE DESCRIPTION:
+                "${problemStatement}"
+
+                RULES:
+                - Return valid JSON only.
+                - Do not include markdown, comments, or explanations.
+                - Do not add extra fields.
+                - If information is missing, make a reasonable legal assumption.
+                - All monetary values must be numbers only.
+                - suggestedIPSSections must be an array of strings.
+
+                RETURN FORMAT:
+                {
+                "predictedCaseType": "",
+                "caseSeverity": "HIGH" | "MEDIUM" | "LOW",
+                "suggestedIPSSections": [],
+                "worstCaseOutcome": "",
+                "estimatedFeeMin": 0,
+                "estimatedFeeMax": 0,
+                "remark": "",
+                "typeOfLawyerNeeded": "Criminal" | "Civil" | "Family" | "Corporate" | "Cyber" | "Property"
+                }
+        
+        `
+
+        //AI Part in API giving prompt and thinking this DATA
+        const ai = new GoogleGenAI({
+            apiKey : "AIzaSyD9sdH_1NiT_ImnzfMOT45qiOsrXzRmzEo"
+        })
+        // AI function import
+        const response = await ai.models.generateContent({
+            model: "gemini-3-flash-preview",
+            contents: prompt,
+            config:{
+                systemInstruction : "Please return data in json format"
+            }
+        });
+
+        return res.status(200).json({
+            result: response;
+            clientCase:clientCase
+        })
 
     } catch (error) {
         return res.status(500).json({
