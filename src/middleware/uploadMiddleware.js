30adf43746390030d790b11a4cf8e63cf9adf0b9
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-02-06 23:42:17 +0530

    multer package function create

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -1,3 +1,23 @@
 import multer from "multer";
 
 
+const storage = multer.diskStorage({
+    destination:function(req, file, cb){
+        cb(null, '/uploads')
+    },
+    filename:function(req, file, cb){
+        const uniqueFileName = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueFileName)
+    }
+})
+
+
+const upload = multer({
+    storage,
+    limits:{
+        fileSize: 1 * 1024 * 1024   //  1 MB
+    }
+})
+
+
+export default upload;
