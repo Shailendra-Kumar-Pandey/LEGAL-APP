d8a6c120b504ab3c51c5b5ae5373b2d7471ec804
Shailendra Kumar Pandey <shailendrakr2010@gmail.com>
2026-04-08 00:07:01 +0530

    fix bugs

--- src/routes/client.js
+++ src/routes/client.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { AiGetInformation, createCaseAIthinking } from '../Controller/clientController.js';
 import { autharized } from '../middleware/autharization.js';
+import upload from '../middleware/uploadMiddleware.js';
 
 
 const router = express.Router()
@@ -9,7 +10,7 @@ const router = express.Router()
 router.get('/AifetchData',autharized, AiGetInformation)
 
 
-router.post('/createCase', autharized, createCaseAIthinking)
+router.post('/createCase', autharized, upload.array("proofFiles", 2), createCaseAIthinking)
 
 
 
